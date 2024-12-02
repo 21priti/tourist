64f4ef22e3c4655e7fd227eacffc00bd6800a210
21priti <pritipralipta18@gmail.com>
2024-12-02 23:52:57 +0530

    ok

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-image:url('./image/bg1.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+    
+</head>
+<body>
+    <?php
+    include_once "navbar.php";
+    ?>
+    <div class="ms-5 pt-3">
+    <!-- <image src="./image/homepic.jpeg"  style="height: 500px; width: 500px; opacity:0.7;"></image> -->
+</div>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-light ">
         <img class="navbar-brand ms-5 rounded-circle" src="./image/logo.jpg" style="height: 125px;">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
