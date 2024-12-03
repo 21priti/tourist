6b6c6e6d5bd21e164a51f48b8b67f7cffc547f56
21priti <pritipralipta18@gmail.com>
2024-12-03 21:00:43 +0530

    packages

--- packages.php
+++ packages.php
@@ -4,26 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Packages</title>
-    <style>
-        *{
-            background: linear-gradient(to bottom, #4FC3F7, #26C6DA);
-        }
-        </style>
+  
+   
 </head>
 <body>
     <?php
     include_once "navbar.php"
     ?>
-    <div class="row ms-5 p-4">
-        <div class="col-md-6">
-            <img src="./image/images (2).jpg" class="img-fluid w-75">
+    <div class="container-fluid rounded shadow-sm">
+        <div class="row p-3">
+            <div class="col-md-6">
+                <img src="./image/images (2).jpg" class="img-fluid w-100">
             </div>
-            <div class="col-md-6 mt-5 ">
-                <h1 class="text-light fw-bold">Discover Your Next Adventure!</h1>
-                <span class="fs-5 fst-italic text-warning">"Welcome to our exclusive packages page—where your dream destinations come to life!
-                     Explore handpicked travel packages crafted for every explorer, from serene getaways to thrilling adventures. 
-                     Let’s make your journey unforgettable!"</span>
+            <div class="col-md-6 py-5">
+                <h1 class="text-primary fw-bold">Discover Your Next Adventure!</h1>
+                <span class="fs-5 fst-italic">
+                    "Welcome to our exclusive packages page—where your dream destinations come to life!
+                        Explore handpicked travel packages crafted for every explorer, from serene getaways to thrilling adventures. 
+                        Let’s make your journey unforgettable!"
+                </span>
+            </div>
         </div>
+    </div>
+    <div class="container">
+        <h1 class="fw-bold fst-italic text-center mt-3 text-decoration-underline pb-4"> Travel To Odisha</h1>
+        <div class="container">
+            <div class="row g-3 border border-1">
+                <div class="col-md-8">
+                    <h4>Puri</h4>
+</div>
+<div class="col-md-4">
+<img src="./image/images (2).jpg" class="img-fluid">
+</div>
+</div>
+    <div class="row border border-1">
+                <div class="col-md-8">
+                    <h4>Puri</h4>
+</div>
+<div class="col-md-4">
+<img src="./image/images (2).jpg" class="img-fluid">
+</div>
+</div>
+
+            <div class="row border border-1">
+                <div class="col-md-8">
+                    <h4>Puri</h4>
+</div>
+<div class="col-md-4">
+<img src="./image/images (2).jpg" class="img-fluid">
+</div>
+</div>
+</div>
+</div>
+
+
         
 </body>
 </html>
