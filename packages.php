a8707bf08759bb3372c68922443b94b81d10db3a
21priti <pritipralipta18@gmail.com>
2024-12-03 19:43:51 +0530

    packages

---
+++ packages.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Packages</title>
+    <style>
+        *{
+            background: linear-gradient(to bottom, #4FC3F7, #26C6DA);
+        }
+        </style>
+</head>
+<body>
+    <?php
+    include_once "navbar.php"
+    ?>
+    <div class="row ms-5 p-4">
+        <div class="col-md-6">
+            <img src="./image/images (2).jpg" class="img-fluid w-75">
+            </div>
+            <div class="col-md-6 mt-5 ">
+                <h1 class="text-light fw-bold">Discover Your Next Adventure!</h1>
+                <span class="fs-5 fst-italic text-warning">"Welcome to our exclusive packages page—where your dream destinations come to life!
+                     Explore handpicked travel packages crafted for every explorer, from serene getaways to thrilling adventures. 
+                     Let’s make your journey unforgettable!"</span>
+        </div>
+        
+</body>
+</html>
