4b9527c664ca69f7965c1865cd6275ce7cb56885
hrusikesh18 <hpadhy99@gmail.com>
2024-12-05 01:50:21 +0530

    follow the structure

--- pages/navbar.php
+++ pages/navbar.php
@@ -4,14 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-      nav{
-        /* background: rgba(104, 175, 199, 0.496); */
-      }
-    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
@@ -40,7 +36,6 @@
           </ul>
           <span class="navbar-text me-3">
             <ul class="navbar-nav">
-            <!-- <a href="#" class="btn btn-primary text-light fs-5">Profile</a> -->
             <li class="nav-item dropdown fw-bold">
               <a class="nav-link dropdown-toggle-d-none " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-fill"></i>
                 Profile
