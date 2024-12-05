22711aed3fd5ba11944900503f943977b81b169e
hrusikesh18 <hpadhy99@gmail.com>
2024-12-06 01:07:06 +0530

    update

--- script/blog.js
+++ script/blog.js
@@ -1,14 +1,15 @@
 const slider = document.querySelector(".slider");
-        const slides = document.querySelectorAll(".container-fluid");
-        let currentIndex = 0;
+const slides = document.querySelectorAll(".container-fluid");
+let currentIndex = 0;
 
-        function slideNext() {
-            const offset = -(currentIndex * 100); // Shift to the next slide
-            slider.style.transform = `translateX(${offset}%)`;
+function slideNext() {
+// Calculate the new translateX value
+    const offset = -(currentIndex * 100);
+    slider.style.transform = `translateX(${offset}%)`;
 
-            // Move to the next slide, loop back at the end
-            currentIndex = (currentIndex + 1) % slides.length;
-        }
+    // Move to the next slide, loop back if at the end
+    currentIndex = (currentIndex + 1) % (slides.length-1);
+}
 
-        // Start auto-slide
-        setInterval(slideNext, 3000); // Change slide every 3 seconds
+// Start the sliding effect with a time interval
+setInterval(slideNext, 3000); // Change slide every 3 seconds
