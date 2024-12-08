5bb5f194ea2026db8d006a41c256b0e95231e9a9
hrusikesh18 <hpadhy99@gmail.com>
2024-12-08 20:42:39 +0530

    home page update

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,92 @@
 <?php include_once "navbar.php"; ?>
+<head>
+    <link rel="stylesheet" href="../style/home.css">
+</head>
+<body>
+    <!-- hero section -->
+    <div>
+        <div id="hero" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="2000">
+                    <img src="../image/hero.webp" class="d-block w-100 image" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1 class="caption-heading">First slide label</h1>
+                        <p class="caption-body">Some representative placeholder content for the first slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="../image/road.webp" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1 class="caption-heading">Second slide label</h1>
+                        <p class="caption-body">Some representative placeholder content for the second slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../image/hill.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1 class="caption-heading">Third slide label</h1>
+                        <p class="caption-body">Some representative placeholder content for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <!-- package  -->
+    <div class="container my-4">
+        <h1 class="text-center mb-4">Packages</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="../image/package1.jpg" class="card-image-top image-card" alt="" srcset="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="../image/package2.jpg" class="card-image-top image-card" alt="" srcset="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="../image/package3.jpg" class="card-image-top image-card" alt="" srcset="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-3">
+            <button type="submit" class="btn btn-primary" value="See More">See More</button>
+        </div>
+    </div>
 
-<!-- hero section -->
-
-
-
-<div id="blog">
-<?php include_once "blog.php"?>
-</div>
-<?php include_once "footer.php"?>
+    
+    <div id="blog">
+    <?php include_once "blog.php"?>
+    </div>
+    <?php include_once "footer.php"?>
+</body>
