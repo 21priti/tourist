219ba0d314f93e2e2b4195b472a2523e98da98ca
21priti <pritipralipta18@gmail.com>
2024-12-15 01:29:49 +0530

    package added form

--- databasefunction/dbfunctions.php
+++ databasefunction/dbfunctions.php
@@ -48,4 +48,52 @@
         }
         
     }
+    function package($pname,$package_no,$sname,$ename,$des1,$des2,$des3,$days,$price,$travelby,$rating){
+        global $conn;
+        try{
+            $qry="INSERT INTO package(name,no_places,starting_place,ending_place,des1,des2,des3,days,price,travel_by,rating) values(?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $conn->prepare($qry);
+            $stmt->bind_param("sisssssidsd",$pname,$package_no,$sname,$ename,$des1,$des2,$des3,$days,$price,$travelby,$rating);
+            $res = $stmt->execute();
+            if(!$res){
+                echo $conn->error;
+                return false;
+            }
+            return $res;
+            
+            } catch(Exception $e){
+                die($e->getMessage());
+            }
+            // } finally {
+            //     $conn->close();
+            // }
+        }
+        // function package_image($pname,$unique_name){
+        //     global $conn;
+        //     try{
+        //         $qry="INSERT INTO picture(package_name,img_path) values(?,?)";
+        //         $stmt = $conn->prepare($qry);
+        //         $stmt->bind_param("ss",$pname,$unique_name);
+        //         if (!$stmt->execute()) {
+        //             $success = false;
+        //             break;
+                
+        //         }else{
+        //             $success = false;
+        //             break;
+        //         }
+        //         if(!$success){
+        //             echo $conn->error;
+        //             return false;
+        //         }
+        //         return true;
+        //     }catch(Exception $e){
+        //             die($e->getMessage());
+        //     }finally {
+        //         $conn->close();
+
+        //     }
+        // }
+
+        
 ?>
