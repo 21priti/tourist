0745578ac6786cb95095e26c4bca096c57bc9a28
hrusikesh18 <hpadhy99@gmail.com>
2024-12-18 20:37:55 +0530

    package fetch from database

--- pages/packages.php
+++ pages/packages.php
@@ -1,6 +1,6 @@
 <?php include_once "navbar.php" ?>
 <body>
-    <div class="container mt-5 pt-5">
+    <div class="container my-5 py-5">
         <h1 class="text-center mb-5">Our Packages</h1>
         <div id="packages-container" class="row g-4">
         
@@ -28,12 +28,12 @@
                     <div class="col-md-4">
                         <div class="card shadow-sm">
                         <img class="card-img-top" src="./uploads/${data[i]['img_path']}" alt="Card image cap" style="height:300px;">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column justify-content-between" style="height:400px;">
                             <h5 class="card-title fw-bold">${data[i]['name']}</h5>
                             <p class="card-text">${data[i]['des1']}</p>
                             <p class="fw-bold mt-2">Number of places - ${data[i]['no_places']}</p>
                             <p class="fw-bold fs-4 mt-2 text-danger">Price: ${data[i]['price']} rs</p>
-                            <a href="package_page.php?pid=${data[i]['id']}" class="btn btn-primary">See Detail</a>
+                            <a href="package_page.php?pid=${data[i]['package_id']}" class="btn btn-primary">See Detail</a>
                         </div>
                         </div>
                     </div>
